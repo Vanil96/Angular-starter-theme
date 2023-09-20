d43bf724cee75b85adb5c3ab247c8931cd245445
Daniel <vanildm@gmail.com>
2023-09-21 00:24:33 +0200

    upd

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,28 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from "@angular/core";
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProfileBarComponent } from './components/profile-bar/profile-bar.component';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { ScrollAnimationDirective } from '@app/core/directives/scroll-animation.directive';
 import { RestrictCharactersDirective } from '@app/core/directives/restrict-characters.directive';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 
 @NgModule({
     declarations: [
         PageNotFoundComponent,
-        NavigationComponent,
         FooterComponent,
         ProfileBarComponent,
         PaginationComponent,
         ScrollAnimationDirective,
         RestrictCharactersDirective,
+        NavbarComponent,
     ],
     imports: [RouterModule, CommonModule],
-    exports: [PageNotFoundComponent, NavigationComponent, FooterComponent, ProfileBarComponent, ScrollAnimationDirective, RestrictCharactersDirective,]
+    exports: [PageNotFoundComponent, FooterComponent, ProfileBarComponent, ScrollAnimationDirective, RestrictCharactersDirective, NavbarComponent,]
 })
 
 export class SharedModule { }
