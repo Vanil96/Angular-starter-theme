c10edf9c8d8a7e99cc4f03e56e73a1237098d68c
Daniel <vanildm@gmail.com>
2023-09-29 16:55:08 +0200

    update material and theme styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -19,6 +19,8 @@ import { MatOptionModule } from '@angular/material/core';
 import { SelectComponent } from './components/form/select/select.component';
 import {MatSelectModule} from '@angular/material/select';
 import { RadioComponent } from './components/form/radio/radio.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     declarations: [
@@ -35,8 +37,8 @@ import { RadioComponent } from './components/form/radio/radio.component';
         SelectComponent,
         RadioComponent,
     ],
-    imports: [RouterModule, CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatAutocompleteModule, MatOptionModule, MatSelectModule ],
-    exports: [PageNotFoundComponent, FooterComponent, ProfileBarComponent, ScrollAnimationDirective, RestrictCharactersDirective, NavbarComponent, InputComponent, FormControlPipe, InputAutocompleteComponent, SelectComponent]
+    imports: [RouterModule, CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatAutocompleteModule, MatOptionModule, MatSelectModule, MatButtonModule, MatIconModule ],
+    exports: [PageNotFoundComponent, FooterComponent, ProfileBarComponent, ScrollAnimationDirective, RestrictCharactersDirective, NavbarComponent, InputComponent, FormControlPipe, InputAutocompleteComponent, SelectComponent, MatButtonModule, MatIconModule ]
 })
 
 export class SharedModule { }
